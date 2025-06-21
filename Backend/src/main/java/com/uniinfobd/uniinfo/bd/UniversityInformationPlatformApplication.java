6130ef2005a9c4c07b49570497551c55e5ce15e8
irfankhansajid <irfankhansajid@gmail.com>
2025-06-21 16:26:09 +0600

    Start

---
+++ Backend/src/main/java/com/uniinfobd/uniinfo/bd/UniversityInformationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.uniinfobd.uniinfo.bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityInformationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityInformationPlatformApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/uniinfobd/uniinfo/bd/UniversityInformationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniinfobd.uniinfo.bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityInformationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
